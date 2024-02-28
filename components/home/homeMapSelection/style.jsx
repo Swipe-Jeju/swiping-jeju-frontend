b0be4de2e318d0822e2900881a6723095e13f2ff
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-02-28 18:36:50 +0900

    ✨Feat: home Map Selection

--- components/home/homeMapSelection/style.jsx
+++ components/home/homeMapSelection/style.jsx
@@ -8,6 +8,37 @@ export const MapContainer = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    padding-left: 2.75rem;
+    padding-right: 2.75rem;
+`;
+
+// MapImage
+export const MapImageContainer = styled.div`
+    width: 40%;
+    height: 260px;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    align-items: center;
+    /* border: 1px solid #ffffff; */
+`;
+
+export const MapTitleContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: start;
+    /* border: 1px solid #ffffff; */
+`;
+export const MapTitle = styled.h1`
+    font-size: 39px;
+    font-weight: 700;
+    color: #ffffff;
+
+    // sapcing gap
+    margin-bottom: 3rem;
+    margin-top: 1rem;
+
+    letter-spacing: 0.1rem;
 `;
 
 export const SelectionButton = styled.button`
