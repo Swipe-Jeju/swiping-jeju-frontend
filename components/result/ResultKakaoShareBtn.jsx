51562424dcc014001bc31e8db5e1497ccb69e0b9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-02-29 03:15:48 +0900

    ✨Feat: 공유 버튼

--- components/result/ResultKakaoShareBtn.jsx
+++ components/result/ResultKakaoShareBtn.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
-import KakaoImage from "../../assets/images/icon/icon-kakao.png";
+
+import KakaoImage from "@/public/images/share/kakao.png";
+import GoogleImage from "@/public/images/share/google.png";
+import FacebookImage from "@/public/images/share/facebook.png";
+import NaverImage from "@/public/images/share/naver.png";
+import * as S from "./style";
 
 const KakaoShareButton = ({ description }) => {
     const shareUrl = typeof window !== "undefined" ? window.location.href : "";
@@ -43,14 +48,40 @@ const KakaoShareButton = ({ description }) => {
     };
 
     return (
-        <div onClick={handleShare}>
-            <Image
-                className="w-10 h-10 cursor-pointer"
-                src={KakaoImage}
-                width={40}
-                alt="카카오톡 공유 이미지"
-            />
-        </div>
+        <S.ShareContainer>
+            <S.ShareButton onClick={handleShare}>
+                <Image
+                    className="w-10 h-10 cursor-pointer"
+                    src={KakaoImage}
+                    width={40}
+                    alt="카카오톡 공유 이미지"
+                />
+            </S.ShareButton>
+            <S.ShareButton onClick={handleShare}>
+                <Image
+                    className="w-10 h-10 cursor-pointer"
+                    src={NaverImage}
+                    width={40}
+                    alt="카카오톡 공유 이미지"
+                />
+            </S.ShareButton>
+            <S.ShareButton onClick={handleShare}>
+                <Image
+                    className="w-10 h-10 cursor-pointer"
+                    src={FacebookImage}
+                    width={40}
+                    alt="카카오톡 공유 이미지"
+                />
+            </S.ShareButton>
+            <S.ShareButton onClick={handleShare}>
+                <Image
+                    className="w-10 h-10 cursor-pointer"
+                    src={GoogleImage}
+                    width={40}
+                    alt="카카오톡 공유 이미지"
+                />
+            </S.ShareButton>
+        </S.ShareContainer>
     );
 };
 
